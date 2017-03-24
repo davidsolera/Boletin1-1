4164f98a2a82c235fae1bfdfc613409ea7b7a2fd
solera <solera@10.2.1.133>
2017-03-24 20:47:15 +0100

    Añadido s ejercicios del 4 al 10 (4 y 10 incluidos).

---
+++ ejercicio8.php
@@ -0,0 +1,22 @@
+<?php
+
+$primera = 1;
+$segunda = 8;
+
+$suma = $primera +$segunda;
+
+$producto =$primera * $segunda;
+
+
+
+if($suma>$producto){
+
+echo "La suma es mayor que el producto.";
+
+}
+
+else{
+
+	echo "el producto es mayor que la suma.";
+
+}
---
+++ ejercicio9.php
@@ -0,0 +1,25 @@
+<?php
+
+$primera = 10;
+$segunda = 9;
+
+$nombre = "David";
+$apellidos = "Garcia Solera";
+
+if($primera>0 && $segunda < $primera){
+
+	echo $nombre;
+}
+
+if($primera > 0  && $segunda >= $primera){
+
+echo $apellidos;
+
+}
+
+if ($primera<0) {
+
+echo "$nombre $apellidos";
+
+
+}
